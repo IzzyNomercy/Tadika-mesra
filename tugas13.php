70eee7e8b3b7b18087482ee98afc687fa6e4f84a
IzzyNomercy <167765591+IzzyNomercy@users.noreply.github.com>
2024-06-25 10:07:24 +0700

    Add files via upload

---
+++ tugas13.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Untitled Document</title>
+</head>
+<body>
+<?
+echo ("Bentuk Pertama");
+?>
+
+<?php
+echo("Bentuk Kedua");
+?>
+
+<script language="php">
+echo("Bentuk Ketiga");
+</script>
+
+<?php
+echo("Bentuk Penulisan Keempat");
+?>
+
+<?php
+//Contoh Komentar Pertama
+/* Contoh Komentar Kedua*/
+echo("Bentuk Penulisan Keempat");
+?>
+
+</body>
+</html>
